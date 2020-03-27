b0840a152ac0274dce75f866b0082249dc245b89
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2020-03-27 00:35:23 -0600

    alazar

--- Proyecto_parcial_1/js/paging.js
+++ Proyecto_parcial_1/js/paging.js
@@ -1,11 +1,11 @@
 $(perfilAdmin.html).ready(function () {
-    $('#table').DataTable();
+    $('#tables').DataTable();
     $('.dataTables_length').addClass('bs-select');
   });
 
   // Basic example
 $(perfilAdmin.html).ready(function () {
-    $('#table').DataTable({
+    $('#tables').DataTable({
       "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
     });
     $('.dataTables_length').addClass('bs-select');
