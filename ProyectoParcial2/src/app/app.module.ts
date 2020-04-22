500d0e7f44d78f49e769fa670c9901990c65c318
moitorres <moi.torres.mQ@gmail.com>
2020-04-21 23:34:06 -0500

    Added components for the header and the footer

--- ProyectoParcial2/src/app/app.module.ts
+++ ProyectoParcial2/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
