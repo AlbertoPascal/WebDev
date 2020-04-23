388079a5f0a31723570988245339aa7787b82798
moitorres <moi.torres.mQ@gmail.com>
2020-04-22 21:14:38 -0500

    Added component for the searchbar

--- ProyectoParcial2/src/app/app.module.ts
+++ ProyectoParcial2/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
