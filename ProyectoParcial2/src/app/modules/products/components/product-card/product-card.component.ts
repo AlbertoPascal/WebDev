cf2aaea370dc73f5f52ddb03fb023d2f866a0986
AlbertoPascal <beto_pascal@hotmail.com>
2020-04-30 00:30:17 -0500

    Fixed product typescript. Now child component stays unary and parent component generates different images with inputs

--- ProyectoParcial2/src/app/modules/products/components/product-card/product-card.component.ts
+++ ProyectoParcial2/src/app/modules/products/components/product-card/product-card.component.ts
@@ -1,21 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
+
 export class ProductCardComponent implements OnInit {
 
-  productos=[
-    {titulo:"Jeans", foto:"../../assets/images/jeans.jpg"},
-    {titulo:"Camisa", foto:"../../assets/images/flannel.jpg"},
-    {titulo:"Playera gris", foto:"../../assets/images/tshirt.jpg"},
-    {titulo:"Playera negra", foto:"../../assets/images/black_tshirt.jpg"}
-  ]
+  @Input() curr_prod ;
+ 
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
--- ProyectoParcial2/src/app/modules/products/components/productos/productos.component.ts
+++ ProyectoParcial2/src/app/modules/products/components/productos/productos.component.ts
@@ -5,8 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.scss']
 })
+
 export class ProductosComponent implements OnInit {
 
+  productos=[
+    {titulo:"Jeans", foto:"../../assets/images/jeans.jpg"},
+    {titulo:"Camisa", foto:"../../assets/images/flannel.jpg"},
+    {titulo:"Playera gris", foto:"../../assets/images/tshirt.jpg"},
+    {titulo:"Playera negra", foto:"../../assets/images/black_tshirt.jpg"}
+  ];
   constructor() { }
 
   ngOnInit(): void {
