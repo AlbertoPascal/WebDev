d3c0e1d0fd9021fe0dbad0ea19950b367b1c3212
moitorres <moi.torres.m@gmail.com>
2020-05-02 16:19:48 -0500

    Moises Torres A01021323: Added snackbar and form control to the savings page

--- ProyectoParcial2/src/app/modules/profile/components/ahorros-savings/ahorros-savings.component.ts
+++ ProyectoParcial2/src/app/modules/profile/components/ahorros-savings/ahorros-savings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar} from '@angular/material/snack-bar';
+import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ahorros-savings',
@@ -7,8 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorrosSavingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar) { }
 
+  openSnackBar(message, action){
+    let snackBarRef = this.snackBar.open(message, action, {duration: 3000});
+  
+    //Cuando el usuario de click en la acción de "deshacer" del snackbar
+    snackBarRef.onAction().subscribe(()=> {
+      console.log("La acción de la snackbar fue activada");
+    })
+  }
+
+  savings = new FormControl('');
+
+  onAdd(){
+    this.savings.reset();
+  }
+  
   ngOnInit(): void {
   }
 
