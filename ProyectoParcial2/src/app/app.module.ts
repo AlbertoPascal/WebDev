f1f45ad0b56260af3864ffe579d8b43439228790
moitorres <moi.torres.m@gmail.com>
2020-05-07 22:41:49 -0500

    Moises Torres - A01021323 - Added markup for all the things that will be translated with internationalization

--- ProyectoParcial2/src/app/app.module.ts
+++ ProyectoParcial2/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,13 +11,14 @@ import { AppRoutingModule} from './app-routing.module';
 import { HomeModule } from './modules/home/home.module';
 import { ProductsModule } from './modules/products/products.module';
 import { ProfileModule } from './modules/profile/profile.module';
-import {NotFoundModule} from './modules/not-found/not-found.module';
+import { NotFoundModule} from './modules/not-found/not-found.module';
 
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { SignupModalComponent } from './main-components/signup-modal/signup-modal.component';
 import { LoginModalComponent } from './main-components/login-modal/login-modal.component';
 
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -44,7 +47,7 @@ import { LoginModalComponent } from './main-components/login-modal/login-modal.c
 })
 export class AppModule { 
   
-
+  
   //possibly need to add here an input variable. The idea is to call the app-component 
 
 }
