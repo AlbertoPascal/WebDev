02c03811c9dd14e784cc488c31ee458af4c4e2c6
AlbertoPascal <beto_pascal@hotmail.com>
2020-05-09 17:27:22 -0500

    Alberto Pascal A01023607 Replaced table for a more user-friendly input. Missing description in form

--- ProyectoParcial2/src/app/modules/profile/components/ahorros-presupuesto/ahorros-presupuesto.component.ts
+++ ProyectoParcial2/src/app/modules/profile/components/ahorros-presupuesto/ahorros-presupuesto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {SaveDataModel} from '../../models/save-data-model.model';
+import { MatSnackBar} from '@angular/material/snack-bar';
+import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ahorros-presupuesto',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorrosPresupuestoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+  openSnackBar(message, action){
+    let snackBarRef = this.snackBar.open(message, action, {duration: 3000});
+  
+    //Cuando el usuario de click en la acción de "deshacer" del snackbar
+    snackBarRef.onAction().subscribe(()=> {
+      console.log("La acción de la snackbar fue activada");
+    })
+  }
+  prep_savings = new SaveDataModel;
+  addCosts = new FormGroup({
+    costs: new FormControl(''),
+  });
+
+  onCost(){
+    this.prep_savings.addCosts(this.addCosts.get('costs').value);
+    alert("Sent " + this.addCosts.get('costs').value + " to save");
+    this.addCosts.reset();
   }
 
 }
