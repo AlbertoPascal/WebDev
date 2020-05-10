c98f4afb71ba23158833870e0df194ecd335be97
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2020-05-09 21:11:25 -0500

    A01024192 Camila animaciones flip cards

--- ProyectoParcial2/src/app/modules/profile/components/flip-card/flip-card.component.ts
+++ ProyectoParcial2/src/app/modules/profile/components/flip-card/flip-card.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-flip-card',
   templateUrl: './flip-card.component.html',
-  styleUrls: ['./flip-card.component.scss']
+  styleUrls: ['./flip-card.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('front', style({
+        transform: 'rotateY(0)'
+        
+      })),
+      state('back', style({
+        transform: 'rotateY(180deg)'
+        
+      })),
+      transition('front=>back', animate('550ms ease-out')),
+      transition('back=>front', animate('550ms ease-in'))
+    ]),
+  ]
+
 })
 export class FlipCardComponent implements OnInit {
 
@@ -12,6 +28,11 @@ export class FlipCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  flip: string = 'front';
+
+  toggleFlip() {
+    this.flip = (this.flip == 'front') ? 'back' : 'front';
   }
 
 }
