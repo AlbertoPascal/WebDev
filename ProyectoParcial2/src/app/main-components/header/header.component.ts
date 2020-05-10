2f8c05c7b2f9d08a3d5d4a038a3f21eb3cbb6a01
CamilaRovirosa <camilarovirosaochoa@gmail.com>
2020-05-09 19:55:32 -0500

    A01024192 Camila animaciones

--- ProyectoParcial2/src/app/main-components/header/header.component.ts
+++ ProyectoParcial2/src/app/main-components/header/header.component.ts
@@ -3,11 +3,29 @@ import { Router} from '@angular/router';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { SignupModalComponent} from '../signup-modal/signup-modal.component';
 import { LoginModalComponent} from '../login-modal/login-modal.component';
-
+import { trigger, state, style, animate, transition } from '@angular/animations';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        //opacity: 0
+        color:  'green',
+        transform: 'scale(1)',
+        
+      })),
+      state('final', style({
+        //opacity: 1
+        color: 'pink',
+        transform: 'scale(1.9)',
+        
+      })),
+      transition('initial=>final', animate('2500ms')),
+      transition('final=>initial', animate('2000ms'))
+    ]),
+  ]
 })
 export class HeaderComponent implements OnInit {
 
@@ -30,12 +48,14 @@ export class HeaderComponent implements OnInit {
   public openLoginModal() {
     this.modal = this.modalService.show(LoginModalComponent);
   }
+  currentState = 'initial';
+  times = 5;
+  counter = 0;
 
-
-
-
-
-
-
-
+  onDone($event) {
+    if (this.counter < this.times) {
+      this.currentState = this.currentState === 'initial' ? 'final' : 'initial';
+      this.counter = 0;
+    }
+  }
 }
