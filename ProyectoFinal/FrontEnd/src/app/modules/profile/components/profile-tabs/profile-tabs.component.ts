948058ebdbd61d727792f9ad965ef44a1edfb121
moitorres <moi.torres.m@gmail.com>
2020-06-12 02:49:11 -0500

    Fixed profile tabs so they detect if the user is admin or no

--- ProyectoFinal/FrontEnd/src/app/modules/profile/components/profile-tabs/profile-tabs.component.ts
+++ ProyectoFinal/FrontEnd/src/app/modules/profile/components/profile-tabs/profile-tabs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { AuthService} from 'src/app/services/auth.service';
+import {UserInfoService} from  'src/app/services/user-info.service'
 
 @Component({
   selector: 'app-profile-tabs',
@@ -8,12 +10,31 @@ import { Router} from '@angular/router';
 })
 export class ProfileTabsComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  isAdmin: boolean = false;
+
+  constructor(public router: Router, public auth: AuthService, public db_user:UserInfoService) { }
   public user_type= localStorage.getItem('user_type');
   ngOnLoad(){
     
   }
   ngOnInit(): void {
+    this.check_admin();
   }
 
+  public check_admin(){
+
+    let subscription = this.auth.getUser$().subscribe((data)=>{
+
+      if(data){
+        let subscription2 = this.db_user.getUser(data.sub).subscribe((data2)=>{
+          this.isAdmin = data2[0].isAdmin;
+
+          subscription2.unsubscribe();
+        })
+      }
+
+      subscription.unsubscribe();
+      return;
+    });
+  }
 }
